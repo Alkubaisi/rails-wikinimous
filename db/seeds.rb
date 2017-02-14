db38435ed8dae78419421dd3f523ec1f62bb408d
fahad alkubaisi <alkubaisi9@gmail.com>
2017-02-14 14:08:20 +0000

    my news

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+
+ Article.create(title: Faker::Book.title, content: Faker::Hipster.paragraph(2, true, 4) )
+  Article.create(title: Faker::Book.title, content: Faker::Hipster.paragraph(2, true, 4) )
+   Article.create(title: Faker::Book.title, content: Faker::Hipster.paragraph(2, true, 4) )
+    Article.create(title: Faker::Book.title, content: Faker::Hipster.paragraph(2, true, 4) )
+     Article.create(title: Faker::Book.title, content: Faker::Hipster.paragraph(2, true, 4) )
+      Article.create(title: Faker::Book.title, content: Faker::Hipster.paragraph(2, true, 4) )
+       Article.create(title: Faker::Book.title, content: Faker::Hipster.paragraph(2, true, 4) )
+        Article.create(title: Faker::Book.title, content: Faker::Hipster.paragraph(2, true, 4) )
+         Article.create(title: Faker::Book.title, content: Faker::Hipster.paragraph(2, true, 4) )
+
